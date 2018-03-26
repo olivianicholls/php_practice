50e35ff164952ab997cd2e10ac891170f53355eb
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2018-03-26 14:02:37 +0100

    Create get.php

---
+++ get.php
@@ -0,0 +1,11 @@
+<?php include "db.php"; ?>
+<?php include "functions.php." ?>
+<?php include "header.php."; ?>
+<?php 
+
+print_r($_GET);
+
+?>
+
+<a href="get.phpi =200">Click Here</a>
+<?php include "footer.php."; ?>
