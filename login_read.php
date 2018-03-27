a6ee838d123739098f86575d9b0ea5305e9a1773
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2018-03-27 13:22:14 +0100

    Update login_read.php

--- login_read.php
+++ login_read.php
@@ -1,31 +1,17 @@
-<?php include "db.php"
-  $query = "SELECT * FROM users";
-  $result = mysqli_query($connection, $query);
-  if (!$result) {
-    die("Query Failed: " . mysqli_error());
-  }
-}
-?>
+<?php include "db.php"; ?>
+<?php include "functions.php." ?>
+<?php include "includes/header.php."; ?>
 
-<html>
- <head lang="en">
-   <meta charset="utf-8">
-   <title>Website</title>
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-   <link rel="stylesheet" href="styles.css">
- </head>
- <body>
-  <div class="container"> 
+  <div class="container">
+      <header class="jumbotron">
+        <h1>Read</h1>
+      </header>
     <div class="co l-xs-6">
+	<pre>
 	<?php 
-	
-	while ($row = mysqli_fetch_assoc($result)) {
-		print_r($row); 
-	}
-	
+	readTable()
 	?>
+	</pre>
 	</div>
   </div>
- </body>
-</html>
-	  
+<?php include "includes/footer.php."; ?>
