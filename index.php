e2bd19239c472bf98cdca541bdce7396a9b53db9
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2018-03-27 13:22:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
-<?php
-if (isset($_POST["submit"])) {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  
-  $connection = mysqli_connect("localhost", "root", "root", "loginapp");
-  if ($connection) {
-    echo "Connected";
-  } else {
-    die("Connection failed");
-  }
-  $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
-  $result = mysqli_query($connection, $query);
-  if (!$result) {
-    die("Query Failed: " . mysqli_error());
-  }
-}
-?>
+<?php include "db.php"; ?>
+<?php include "functions.php."; ?>
+<?php createUser();?>
+<?php include "includes/header.php."; ?>
 
-<html>
-  <head lang="en">
-    <meta charset="utf-8">
-    <title>Website</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
     <div class="container">
       <header class="jumbotron">
-        <h1>SPAN&PLAN</h1>
+        <h1>Create</h1>
       </header>
-      <h3>Description of stuff</h3>
       <div class="col-xs-4"></div>
       <div class="col-xs-4">
         <form action="index.php" method="post">
@@ -47,5 +24,6 @@ if (isset($_POST["submit"])) {
       </div>
       <div class="col-xs-4"></div>
     </div>
-  </body>
-</html>
+	
+	<?php include "includes/footer.php."; ?>
+  
